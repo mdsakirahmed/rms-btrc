1539065b09af7c242f0a132e2fb64c310bac80fa
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-12-13 11:23:27 +0600

    Update left sitebar

--- resources/views/layouts/backend/partials/left-sidebar.blade.php
+++ resources/views/layouts/backend/partials/left-sidebar.blade.php
@@ -6,7 +6,9 @@
             <div class="user-pro-body">
                 <div><img src="assets/images/users/2.jpg" alt="user-img" class="img-circle"></div>
                 <div class="dropdown">
-                    <a href="javascript:void(0)" class="dropdown-toggle u-dropdown link hide-menu" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Steave Gection <span class="caret"></span></a>
+                    <a href="javascript:void(0)" class="dropdown-toggle u-dropdown link hide-menu"
+                        data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Steave
+                        Gection <span class="caret"></span></a>
                     <div class="dropdown-menu animated flipInY">
                         <!-- text-->
                         <a href="javascript:void(0)" class="dropdown-item"><i class="ti-user"></i> My Profile</a>
@@ -17,11 +19,13 @@
                         <!-- text-->
                         <div class="dropdown-divider"></div>
                         <!-- text-->
-                        <a href="javascript:void(0)" class="dropdown-item"><i class="ti-settings"></i> Account Setting</a>
+                        <a href="javascript:void(0)" class="dropdown-item"><i class="ti-settings"></i> Account
+                            Setting</a>
                         <!-- text-->
                         <div class="dropdown-divider"></div>
                         <!-- text-->
-                        <a href="javascript:void(0)" class="dropdown-item logout-btn"><i class="fa fa-power-off"></i> Logout</a>
+                        <a href="javascript:void(0)" class="dropdown-item logout-btn"><i class="fa fa-power-off"></i>
+                            Logout</a>
                         <!-- text-->
                     </div>
                 </div>
@@ -31,38 +35,52 @@
         <nav class="sidebar-nav">
             <ul id="sidebarnav">
                 <li class="nav-small-cap">--- BTRC</li>
-                <li> <a class="waves-effect waves-dark" href="{{ route('dashboard') }}" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Dashboard</span></a></li>
-                @can('manage user permission')
-                <li> <a class="waves-effect waves-dark" href="{{ route('user-permission.index') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">Role & Permission</span></a></li>
-                <li> <a class="waves-effect waves-dark" href="{{ route('role.index') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">Role Management</span></a></li>
-                @endcan
-                @can('product list')
-                <li> <a class="waves-effect waves-dark" href="{{ route('product.index') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">Product</span></a></li>
-                @endcan  
+                <li> <a class="waves-effect waves-dark" href="{{ route('dashboard') }}" aria-expanded="false"><i
+                            class="icon-speedometer"></i><span class="hide-menu">Dashboard</span></a></li>
                 @can('user')
-                <li> <a class="waves-effect waves-dark" href="{{ route('user') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">User</span></a></li>
-                @endcan  
+                <li> <a class="waves-effect waves-dark" href="{{ route('user') }}" aria-expanded="false"><i
+                            class="far fa-circle text-info"></i><span class="hide-menu">User</span></a></li>
+                @endcan
                 @can('document')
-                <li> <a class="waves-effect waves-dark" href="{{ route('document') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">Document</span></a></li>
-                @endcan  
-                @can('license category')
-                <li> <a class="waves-effect waves-dark" href="{{ route('licenseCategory') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">License Category</span></a></li>
-                @endcan  
-                @can('license sub category')
-                <li> <a class="waves-effect waves-dark" href="{{ route('licenseSubCategory') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">License Sub Category</span></a></li>
-                @endcan  
-                @can('operator')
-                <li> <a class="waves-effect waves-dark" href="{{ route('operator') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">Operator</span></a></li>
-                @endcan  
-                @can('receiver-fee')
-                <li> <a class="waves-effect waves-dark" href="{{ route('receiver-fee') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">Receiver-fee</span></a></li>
-                @endcan  
-                @can('receiver-period')
-                <li> <a class="waves-effect waves-dark" href="{{ route('receiver-period') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">Receiver-period</span></a></li>
-                @endcan  
+                <li> <a class="waves-effect waves-dark" href="{{ route('document') }}" aria-expanded="false"><i
+                            class="far fa-circle text-info"></i><span class="hide-menu">Document</span></a></li>
+                @endcan
+
                 @can('payment receive')
-                <li> <a class="waves-effect waves-dark" href="{{ route('paymentReceive') }}" aria-expanded="false"><i class="far fa-circle text-info"></i><span class="hide-menu">Payment Receive</span></a></li>
-                @endcan  
+                <li> <a class="waves-effect waves-dark" href="{{ route('paymentReceive') }}" aria-expanded="false"><i
+                            class="far fa-circle text-info"></i><span class="hide-menu">Payment Receive</span></a></li>
+                @endcan
+                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
+                            class="ti-layout-media-right-alt"></i><span class="hide-menu">Permission</span></a>
+                    <ul aria-expanded="false" class="collapse">
+                        @can('manage user permission')
+                        <li> <a href="{{ route('user-permission.index') }}">License Category</a></li>
+                        @endcan
+                        @can('product list')
+                        <li> <a href="{{ route('role.index') }}">License Sub Category</a></li>
+                        @endcan
+                    </ul>
+                </li>
+                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
+                            class="ti-layout-media-right-alt"></i><span class="hide-menu">Setting</span></a>
+                    <ul aria-expanded="false" class="collapse">
+                        @can('license category')
+                        <li> <a href="{{ route('licenseCategory') }}">License Category</a></li>
+                        @endcan
+                        @can('license sub category')
+                        <li> <a href="{{ route('licenseSubCategory') }}">License Sub Category</a></li>
+                        @endcan
+                        @can('operator')
+                        <li> <a href="{{ route('operator') }}">Operator</a></li>
+                        @endcan
+                        @can('receiver-fee')
+                        <li> <a href="{{ route('receiver-fee') }}">Receiver-fee</a></li>
+                        @endcan
+                        @can('receiver-period')
+                        <li> <a href="{{ route('receiver-period') }}">Receiver-period</a></li>
+                        @endcan
+                    </ul>
+                </li>
             </ul>
         </nav>
         <!-- End Sidebar navigation -->

af66fb2294be52c84253bd960650be3737bedd13
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-12-19 18:15:24 +0600

    permission seed

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -22,17 +22,17 @@ class RoleAndPermissionSeeder extends Seeder
         //Permission
         Permission::create(['name' => 'my-dashboard'])->assignRole($admin_role);
         Permission::create(['name' => 'permission-management'])->assignRole($admin_role);
-        Permission::create(['name' => 'product list'])->assignRole($admin_role);
-        Permission::create(['name' => 'product export'])->assignRole($admin_role);
-        Permission::create(['name' => 'product import'])->assignRole($admin_role);
+        Permission::create(['name' => 'product-list'])->assignRole($admin_role);
+        Permission::create(['name' => 'product-export'])->assignRole($admin_role);
+        Permission::create(['name' => 'product-import'])->assignRole($admin_role);
         Permission::create(['name' => 'user'])->assignRole($admin_role);
         Permission::create(['name' => 'document'])->assignRole($admin_role);
         Permission::create(['name' => 'license-category'])->assignRole($admin_role);
-        Permission::create(['name' => 'license sub category'])->assignRole($admin_role);
+        Permission::create(['name' => 'license-sub-category'])->assignRole($admin_role);
         Permission::create(['name' => 'operator'])->assignRole($admin_role);
         Permission::create(['name' => 'receiver-fee'])->assignRole($admin_role);
         Permission::create(['name' => 'receiver-period'])->assignRole($admin_role);
-        Permission::create(['name' => 'payment receive'])->assignRole($admin_role);
+        Permission::create(['name' => 'payment-receive'])->assignRole($admin_role);
         Permission::create(['name' => 'license'])->assignRole($admin_role);
         Permission::create(['name' => 'report'])->assignRole($admin_role);
     }

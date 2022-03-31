9fb5f21da1fbb3267e9e860779998488571bacf5
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2022-03-31 14:45:01 +0600

    Update ReportController.php

--- app/Http/Controllers/Backend/ReportController.php
+++ app/Http/Controllers/Backend/ReportController.php
@@ -26,46 +26,25 @@ class ReportController extends Controller
                 return request()->pay_order_number ? $query->from('partial_payments')->where('pay_order_number', request()->pay_order_number) : '';
             })->where(function ($query) {
                 return request()->bank ? $query->from('partial_payments')->where('bank', request()->bank) : '';
-            })
-            ->with(['payment' => function ($payment) {
+            })->with(['payment' => function ($payment) {
                 $payment->with(['expiration' => function ($expiration) {
                     $expiration->with(['operator' => function ($operator) {
-                        $operator->with(['category', 'sub_category']);
+                        if(request()->operator){
+                            $operator->where('name', request()->operator);
+                        }
+                        $operator->with(['category' => function($category){
+                            if(request()->category){
+                                $category->where('name', request()->category);
+                            }
+                        }]);
+                        $operator->with(['sub_category' => function($sub_category){
+                            if(request()->sub_category){
+                                $sub_category->where('name', request()->sub_category);
+                            }
+                        }]);
                     }]);
                 }]);
-            }])
-
-
-            ->get();
-
-        //     $data  = Truck::where( function($query) use($request){
-        //         return $request->filter_brand ?
-        //                $query->from('trucks')->where('brand_id',$request->filter_brand) : '';
-        //    })
-
-        // $data_set = DB::table('partial_payments')
-        // ->join('banks', 'partial_payments.bank_id', '=', 'banks.id')
-        // // ->join('payments', 'partial_payments.payment_id', '=', 'payments.id')
-        // // ->join('expirations', 'payments.expiration_id', '=', 'expirations.id')
-        // // ->join('operators', 'expirations.operator_id', '=', 'operators.id')
-        // // ->join('license_categories', 'operators.category_id', '=', 'license_categories.id')
-        // // ->join('license_sub_categories', 'operators.sub_category_id', '=', 'license_sub_categories.id')
-        // ->select('operators.name as operator_name', 'banks.id as bank_id', 'banks.name as bank_name', 'pay_order_number')
-        // ->where(function ($query) {
-        //     dd($query->from('banks'));
-        //     return request()->operator ? $query->from('operators')->where('operator_name', request()->operator) : '';
-        // })
-        // ->where(function ($query) {
-        //     return request()->bank ? $query->from('banks')->where('bank_id', request()->bank) : '';
-        // })
-        // ->where(function ($query) {
-        //     return request()->pay_order_number ? $query->from('partial_payments')->where('pay_order_number', request()->pay_order_number) : '';
-        // })
-        // ->get();
-
-        // dd(request()->operator);
-
-        // dd($data_set);
+            }])->get();
 
         return view('backend.report', [
             'banks' => Bank::latest()->get(),

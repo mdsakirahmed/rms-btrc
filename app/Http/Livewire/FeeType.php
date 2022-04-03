ecd08dd3ae6eaf228a22ef9fa393528e1f9aea98
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2022-04-03 14:44:15 +0600

    Fee type crud

--- app/Http/Livewire/FeeType.php
+++ app/Http/Livewire/FeeType.php
@@ -2,14 +2,46 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\FeeType as ModelsFeeType;
 use Livewire\Component;
 
 class FeeType extends Component
 {
+    public $name;
+    public $fee_type;
+
+    public function create(){
+        $this->name = $this->fee_type = null;
+    }
+
+    public function submit(){
+        $validate_data = $this->validate([
+            'name' => 'required'
+        ]);
+        if($this->fee_type){
+            $this->fee_type->update($validate_data);
+        }else{
+            ModelsFeeType::create($validate_data);
+        }
+        $this->create();
+        $this->dispatchBrowserEvent('alert', ['type' => 'success',  'message' => 'Success !']);
+    }
+
+    public function select_for_edit(ModelsFeeType $fee_type){
+        $this->name = $fee_type->name;
+        $this->fee_type = $fee_type;
+    }
+
+    public function delete(ModelsFeeType $fee_type){
+        $fee_type->delete();
+        $this->dispatchBrowserEvent('alert', ['type' => 'success',  'message' => 'Success !']);
+    }
+
     public function render()
     {
-        return view('livewire.fee-type')
-        ->extends('layouts.backend.app', ['title' => 'Fee type'])
+        return view('livewire.fee-type', [
+            'fee_types' => ModelsFeeType::all()
+        ])->extends('layouts.backend.app', ['title' => 'Fee type'])
         ->section('content');
     }
 }
--- resources/views/livewire/fee-type.blade.php
+++ resources/views/livewire/fee-type.blade.php
@@ -1,3 +1,87 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    <div class="row page-titles">
+        <div class="col-md-5 align-self-center">
+            <h4 class="text-themecolor">Fee type Page</h4>
+        </div>
+        <div class="col-md-7 align-self-center text-end">
+            <div class="d-flex justify-content-end align-items-center">
+                <ol class="breadcrumb justify-content-end">
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                    <li class="breadcrumb-item active">Fee type Page</li>
+                </ol>
+                <button type="button" wire:click="create" class="btn btn-dark d-none d-lg-block m-l-15" alt="default" data-bs-toggle="modal" data-bs-target=".bs-example-modal-lg"><i class="fa fa-plus-circle"></i> Create New</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table color-table success-table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th style="text-align: center;">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($fee_types as $fee_type)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>
+                                        {{ $fee_type->name }} &nbsp;
+                                    </td>
+                                    <td style="text-align: center;">
+                                        <button type="button" class="btn btn-primary" wire:click="select_for_edit({{ $fee_type->id }})" alt="default" data-bs-toggle="modal" data-bs-target=".bs-example-modal-lg">Edit</button>
+                                        <button type="button" class="btn btn-danger text-white" wire:click="delete({{ $fee_type->id }})" onclick="confirm('Are you sure you want to remove ?') || event.stopImmediatePropagation()"> Delete </button>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <!-- sample modal content -->
+            <div wire:ignore.self class="modal bs-example-modal-lg fade" tabindex="-1" data-backdrop="static" role="dialog" aria-labelledby="" aria-hidden="true" style="display: none;">
+                <div class="modal-dialog modal-xl modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header bg-primary text-white">
+                            <h4 class="modal-title" id="">Fee type Form</h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form wire:submit.prevent="submit">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="name" wire:model="name" placeholder="Enter Name here">
+                                            <label for="name">Name</label>
+                                        </div>
+                                        <x-error name="name" />
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-md-flex align-items-center mt-3">
+                                            <div class="ms-auto mt-3 mt-md-0">
+                                                <button type="submit" class="btn btn-success text-white">Submit</button>
+                                                <button type="button" class="btn btn-danger waves-effect text-start text-white" data-bs-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+            <!-- /.modal -->
+        </div>
+    </div>
 </div>

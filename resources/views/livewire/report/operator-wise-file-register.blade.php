e7d9f606d646929b5520f611597cb03a9354fac3
Md. Sakir Ahmed <52326976+dt-sakir@users.noreply.github.com>
2022-04-12 13:14:39 +0600

    Update operator-wise-file-register.blade.php

--- resources/views/livewire/report/operator-wise-file-register.blade.php
+++ resources/views/livewire/report/operator-wise-file-register.blade.php
@@ -15,11 +15,8 @@
     <div class="row"> 
         <div class="col-12">
             <div class="card">
-                <div class="card-header">
-                    <h1>#Payments</h1>
-                </div>
                 <div class="card-body">
-                    <h4 class="card-title">Recives</h4>
+                    <h4 class="card-title"># Payments</h4>
                     <div class="table-responsive">
                         <table class="table color-table primary-table" style="font-size: 8px;">
                             <thead>

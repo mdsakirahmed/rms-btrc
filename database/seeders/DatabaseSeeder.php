5a8cf7f976ad55c837de2e9090fb447640879906
Md. Sakir Ahmed <52326976+dt-sakir@users.noreply.github.com>
2022-05-11 13:10:21 +0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Operator::factory(5000)->create();
+        Operator::factory(500)->create();
         $this->call(RoleAndPermissionSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(FeeTypeSeeder::class);

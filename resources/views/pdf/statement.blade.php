b5da2cd8456d018b58aca8f12d177119945019ad
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-05-23 11:51:11 +0600

    Total and In Crore

--- resources/views/pdf/statement.blade.php
+++ resources/views/pdf/statement.blade.php
@@ -13,6 +13,12 @@
             </tr>
         </thead>
         <tbody>
+            @php
+                $total_receivable = 0;
+                $total_paid_amount = 0;
+                $total_due_amount = 0;
+                $fee_type_wise_total_due_amount = 0;
+            @endphp
             @foreach ($collections as $collection)
                 <tr @if($loop->odd) style="background: #F7F4F3" @endif>
                     <th scope="row">{{ $loop->iteration }}</th>
@@ -22,7 +28,31 @@
                     <td style="text-align: right">{{ money_format_india($collection->total_due_amount()) }}</td>
                     <td style="text-align: right">{{ money_format_india($collection->expiration->fee_type_wise_total_due_amount($selected_fee_type)) }}</td>
                 </tr>
+                @php
+                    $total_receivable += $collection->total_receivable;
+                    $total_paid_amount += $collection->total_paid_amount();
+                    $total_due_amount += $collection->total_due_amount();
+                    $fee_type_wise_total_due_amount += $collection->expiration->fee_type_wise_total_due_amount($selected_fee_type);
+                @endphp
             @endforeach
         </tbody>
+        <tfoot>
+            <tr style="background: #f0beae">
+                <th scope="row"></th>
+                <th>TOTAL</th>
+                <td style="text-align: right">{{ money_format_india($total_receivable) }}</td>
+                <td style="text-align: right">{{ money_format_india($total_paid_amount) }}</td>
+                <td style="text-align: right">{{ money_format_india($total_due_amount) }}</td>
+                <td style="text-align: right">{{ money_format_india($fee_type_wise_total_due_amount) }}</td>
+            </tr>
+            <tr style="background: #f1dad2ec">
+                <th scope="row"></th>
+                <th>In Crore </th>
+                <td style="text-align: right">{{ $total_receivable/10000000 }}</td>
+                <td style="text-align: right">{{ $total_paid_amount/10000000 }}</td>
+                <td style="text-align: right">{{ $total_due_amount/10000000 }}</td>
+                <td style="text-align: right">{{ $fee_type_wise_total_due_amount/10000000 }}</td>
+            </tr>
+        </tfoot>
     </table>
 @endsection

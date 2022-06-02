068ed03bd959c3f47c75824c7ba2cdc3fe9cc902
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-06-02 11:06:59 +0600

    Permission wise left menu show

--- resources/views/layouts/backend/partials/header.blade.php
+++ resources/views/layouts/backend/partials/header.blade.php
@@ -11,8 +11,11 @@
         <!-- ============================================================== -->
         <div class="navbar-collapse">
             <ul class="navbar-nav me-auto">
-                <li class="nav-item"> <a class="nav-link nav-toggler d-block d-md-none waves-effect waves-dark" href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
-                <li class="nav-item"> <a class="nav-link sidebartoggler d-none d-lg-block d-md-block waves-effect waves-dark" href="javascript:void(0)"><i class="icon-menu"></i></a> </li>
+                <li class="nav-item"><a class="nav-link nav-toggler d-block d-md-none waves-effect waves-dark"
+                                        href="javascript:void(0)"><i class="ti-menu"></i></a></li>
+                <li class="nav-item"><a
+                        class="nav-link sidebartoggler d-none d-lg-block d-md-block waves-effect waves-dark"
+                        href="javascript:void(0)"><i class="icon-menu"></i></a></li>
             </ul>
             <!-- User profile and search -->
             <!-- ============================================================== -->
@@ -21,8 +24,9 @@
                 <!-- Comment -->
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown" style="margin-right: 20px;">
-                    <a class="nav-link dropdown-toggle waves-effect waves-dark" href="" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="ti-user"></i>
-                        <div class="notify"> <span class="heartbit"></span> <span class="point"></span> </div>
+                    <a class="nav-link dropdown-toggle waves-effect waves-dark" href="" data-bs-toggle="dropdown"
+                       aria-haspopup="true" aria-expanded="false"> <i class="ti-user"></i>
+                        <div class="notify"><span class="heartbit"></span> <span class="point"></span></div>
                     </a>
                     <div class="dropdown-menu dropdown-menu-end mailbox animated bounceInDown">
                         <ul>
@@ -31,17 +35,23 @@
                                     <div class="card-body">
                                         <div class="row align-items-center">
                                             <div class="col-md-4 col-lg-3 text-center">
-                                                <a href="{{ route('profile') }}"><img src="{{ asset(auth()->user()->image ?? 'assets/images/users/2.jpg') }}" width="90" alt="user" class="img-circle img-fluid"></a>
+                                                <a href="{{ route('profile') }}"><img
+                                                        src="{{ asset(auth()->user()->image ?? 'assets/images/users/2.jpg') }}"
+                                                        width="90" alt="user" class="img-circle img-fluid"></a>
                                             </div>
                                             <div class="col-md-8 col-lg-9">
-                                                <h3 class="box-title m-b-0">{{ auth()->user()->name }}</h3> <small>{{ auth()->user()->email }}</small>
+                                                <h3 class="box-title m-b-0">{{ auth()->user()->name }}</h3>
+                                                <small>{{ auth()->user()->email }}</small>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </li>
                             <li class="text-center">
-                                <a class="nav-link text-center link" href="{{ route('profile') }}"> <strong>Profile Setting</strong> <i class="fa fa-angle-right"></i> </a>
+                                @can('profile')
+                                    <a class="nav-link text-center link" href="{{ route('profile') }}"> <strong>Profile
+                                            Setting</strong> <i class="fa fa-angle-right"></i> </a>
+                                @endcan
                                 <a href="javascript:void(0);" class="btn btn-primary logout-btn">Logout</a>
                             </li>
                         </ul>
--- resources/views/layouts/backend/partials/left-sidebar.blade.php
+++ resources/views/layouts/backend/partials/left-sidebar.blade.php
@@ -1,7 +1,8 @@
 <aside class="left-sidebar">
     <!-- Sidebar scroll-->
     <div style="width: 100%;" class="text-center">
-        <img src="{{ asset('assets/frontend/images/logo.png') }}" class="mb-1 p-2 bg-white" style="max-width: 80%; height: auto;">
+        <img src="{{ asset('assets/frontend/images/logo.png') }}" class="mb-1 p-2 bg-white"
+             style="max-width: 80%; height: auto;">
         <a class="navbar-brand" href="{{ route('dashboard') }}"><span>
                 <p style="font-size: 12px; color:black;">Bangladesh Telecommunication <br> Regulatory Commission (BTRC)</p>
             </span>
@@ -11,8 +12,10 @@
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
             <ul id="sidebarnav">
-                <li><a class="waves-effect waves-dark" href="{{ route('dashboard') }}" aria-expanded="false"><i
-                            class="icon-speedometer"></i><span class="hide-menu">Dashboard</span></a></li>
+                @can('my-dashboard')
+                    <li><a class="waves-effect waves-dark" href="{{ route('dashboard') }}" aria-expanded="false"><i
+                                class="icon-speedometer"></i><span class="hide-menu">Dashboard</span></a></li>
+                @endcan
                 @can('application')
                     <li><a class="waves-effect waves-dark" href="{{ route('application') }}" aria-expanded="false">
                             <i class="mdi mdi-file-document"></i><span class="hide-menu">Application</span></a></li>
@@ -22,43 +25,46 @@
                                 class="mdi mdi-clipboard-account"></i><span class="hide-menu">Operator</span></a></li>
                 @endcan
                 @can('payment')
-                    <li class="{{ request()->is('payment*') ? 'active' : '' }}"><a
-                            class="waves-effect waves-dark {{ request()->is('payment*') ? 'active' : '' }}"
-                            href="{{ route('payment') }}" aria-expanded="false"><i
+                    <li class="{{ request()->is('payment*') ? 'active' : '' }}">
+                        <a class="waves-effect waves-dark" href="{{ route('payment') }}" aria-expanded="false"><i
                                 class="mdi mdi-cash"></i><span class="hide-menu">Collection</span></a></li>
                 @endcan
                 @can('document')
                     <li><a class="waves-effect waves-dark" href="{{ route('document') }}" aria-expanded="false"><i
                                 class="mdi mdi-file-pdf"></i><span class="hide-menu">Document</span></a></li>
                 @endcan
-                <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
-                            class="mdi mdi-human-greeting"></i><span class="hide-menu">User Access</span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        @can('user')
-                            <li><a href="{{ route('user') }}">User</a></li>
-                        @endcan
-                        @can('permission-management')
-                            <li><a href="{{ route('permission-management') }}">Permission Management</a></li>
-                        @endcan
-                    </ul>
-                </li>
-                <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
-                            class="mdi mdi-settings"></i><span class="hide-menu">Setting</span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        @can('bank')
-                            <li><a href="{{ route('bank') }}">Bank</a></li>
-                        @endcan
-                        @can('fee-type')
-                            <li><a href="{{ route('fee-type') }}">Fee Type</a></li>
-                        @endcan
-                        @can('license-category')
-                            <li><a href="{{ route('license-category') }}">Category</a></li>
-                        @endcan
-                        @can('license-sub-category')
-                            <li><a href="{{ route('license-sub-sategory') }}">Sub-Category</a></li>
-                        @endcan
-                    </ul>
-                </li>
+                @canany(['user', 'permission-management'])
+                    <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
+                                class="mdi mdi-human-greeting"></i><span class="hide-menu">User Access</span></a>
+                        <ul aria-expanded="false" class="collapse">
+                            @can('user')
+                                <li><a href="{{ route('user') }}">User</a></li>
+                            @endcan
+                            @can('permission-management')
+                                <li><a href="{{ route('permission-management') }}">Permission Management</a></li>
+                            @endcan
+                        </ul>
+                    </li>
+                @endcanany
+                @canany(['bank', 'fee-type', 'license-category', 'license-sub-category'])
+                    <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
+                                class="mdi mdi-settings"></i><span class="hide-menu">Setting</span></a>
+                        <ul aria-expanded="false" class="collapse">
+                            @can('bank')
+                                <li><a href="{{ route('bank') }}">Bank</a></li>
+                            @endcan
+                            @can('fee-type')
+                                <li><a href="{{ route('fee-type') }}">Fee Type</a></li>
+                            @endcan
+                            @can('license-category')
+                                <li><a href="{{ route('license-category') }}">Category</a></li>
+                            @endcan
+                            @can('license-sub-category')
+                                <li><a href="{{ route('license-sub-sategory') }}">Sub-Category</a></li>
+                            @endcan
+                        </ul>
+                    </li>
+                @endcanany
                 @can('report')
                     <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
                                 class="mdi mdi-file-find"></i><span class="hide-menu">Report</span></a>

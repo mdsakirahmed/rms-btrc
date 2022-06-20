6cc9faa20833e58e17621b67b81c5c21e40d8c51
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-06-20 15:10:01 +0600

    Update ReportSix.php

--- app/Http/Livewire/Report/ReportSix.php
+++ app/Http/Livewire/Report/ReportSix.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire\Report;
 
+use App\Models\LicenseCategory;
+use App\Models\LicenseSubCategory;
+use App\Models\Operator;
 use Livewire\Component;
 
 class ReportSix extends Component
 {
     public function render()
     {
-        return view('livewire.report.report-six');
+        return view('livewire.report.report-six', [
+            'categories' => LicenseCategory::all(),
+            'sub_categories' => LicenseSubCategory::all(),
+            'operators' => Operator::where('category_id', $this->category)->where('sub_category_id', $this->sub_category)->get(),
+        ])->extends('layouts.backend.app', ['title' => 'Revenue Sharing Statement'])->section('content');
     }
 }

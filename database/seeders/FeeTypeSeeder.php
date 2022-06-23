90b431ecafa17548a5b6bc67d1ede21a39e68634
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-06-23 17:11:43 +0600

    Update FeeTypeSeeder.php

--- database/seeders/FeeTypeSeeder.php
+++ database/seeders/FeeTypeSeeder.php
@@ -22,11 +22,7 @@ class FeeTypeSeeder extends Seeder
             'period_format' => 1,
             'schedule_include_to_beginning_of_period' => true
         ]);
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 1,
-            'ending_month' => 12,
-        ]);
+
 
         //1
         $fee_type =  FeeType::create([
@@ -36,46 +32,7 @@ class FeeTypeSeeder extends Seeder
             'period_format' => 2,
             'schedule_include_to_beginning_of_period' => false
         ]);
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 1,
-            'ending_month' => 2,
-        ]);
 
-        //2
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 3,
-            'ending_month' => 4,
-        ]);
-
-        //3
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 5,
-            'ending_month' => 6,
-        ]);
-
-        //4
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 7,
-            'ending_month' => 8,
-        ]);
-
-        //5
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 9,
-            'ending_month' => 10,
-        ]);
-
-        //6
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 11,
-            'ending_month' => 12,
-        ]);
 
         $fee_type = FeeType::create([
             'name' => 'Spectrum Charge', 
@@ -83,30 +40,6 @@ class FeeTypeSeeder extends Seeder
             'schedule_month' => 0, 
             'period_format' => 2,
             'schedule_include_to_beginning_of_period' => false
-        ]);
-        //1
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 1,
-            'ending_month' => 3,
-        ]);
-        //2
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 4,
-            'ending_month' => 6,
-        ]);
-        //3
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 7,
-            'ending_month' => 9,
-        ]);
-        //4
-        FeeTypeWisePeriod::create([
-            'fee_type_id' => $fee_type->id,
-            'starting_month' => 10,
-            'ending_month' => 12,
         ]);
 
         $fee_type = FeeType::create([

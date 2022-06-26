b4ba5cb44a452e436d56615d09dc1e5931119fbe
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-06-26 17:20:02 +0600

    Update DashboardCard.php

--- app/Models/DashboardCard.php
+++ app/Models/DashboardCard.php
@@ -9,4 +9,51 @@ class DashboardCard extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            // ... code here;
+        });
+
+        self::created(function ($model) {
+            activity()
+                // ->causedBy($userModel)
+                ->performedOn($model)
+                ->useLog("create")
+                ->log('Create dashboard card');
+        });
+
+        self::updating(function ($model) {
+            // ... code here
+        });
+
+        self::updated(function ($model) {
+            $changes = $model->isDirty() ? $model->getDirty() : false;
+            if ($changes) {
+                foreach ($changes as $attr => $value) {
+                    activity()
+                        // ->causedBy($user)
+                        ->performedOn($model)
+                        ->useLog("edit")
+                        ->log("Update dashboard card : $attr from {$model->getOriginal($attr)} to {$model->$attr}");
+                }
+            }
+        });
+
+        self::deleting(function ($model) {
+            // ... code here
+        });
+
+        self::deleted(function ($model) {
+            activity()
+                // ->causedBy($userModel)
+                ->performedOn($model)
+                ->useLog("delete")
+                ->withProperties(['record' => $model])
+                ->log('Delete dashboard card');
+        });
+    }
 }

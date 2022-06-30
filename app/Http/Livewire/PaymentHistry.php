c118b4e59a8350b0f48298e5ad93d5b733e46708
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-06-30 13:50:24 +0600

    Update PaymentHistry.php

--- app/Http/Livewire/PaymentHistry.php
+++ app/Http/Livewire/PaymentHistry.php
@@ -8,33 +8,37 @@ use Livewire\Component;
 class PaymentHistry extends Component
 {
     public $selected_payment_for_delete;
+
     public function render()
     {
-        return view('livewire.payment-histry',[
-            'payments' => Payment::where(function($query){
-                if(!auth()->user()->can('master')){
+        return view('livewire.payment-histry', [
+            'payments' => Payment::where(function ($query) {
+                if (!auth()->user()->can('master')) {
                     $query->where('created_by', auth()->user()->id);
                 }
             })->latest()->paginate(10)
         ])
-        ->extends('layouts.backend.app', ['title' => 'Payment History'])
-        ->section('content');
+            ->extends('layouts.backend.app', ['title' => 'Payment History'])
+            ->section('content');
     }
 
-    public function select_for_delete(Payment $payment){
+    public function select_for_delete(Payment $payment)
+    {
         $this->selected_payment_for_delete = $payment;
     }
 
-    public function delete(){
-        if($this->payment->created_at->diffInDays()<=3 || auth()->user()->can('master')) {
-            if($this->selected_payment_for_delete){
-                $this->selected_payment_for_delete->delete();
-                $this->dispatchBrowserEvent('alert', ['type' => 'success',  'message' => 'Success !']);
-            }else{
-                $this->dispatchBrowserEvent('alert', ['type' => 'error',  'message' => 'Selected payment not found !']);
+    public function delete()
+    {
+        if (!auth()->user()->can('master')) {
+            if ($this->payment->created_at->diffInDays() > 3 ||  $this->selected_payment_for_delete->created_by != auth()->user()->id) {
+                abort(404);
             }
-        }else{
-            $this->dispatchBrowserEvent('alert', ['type' => 'error', 'message' => 'Action Disabled !']);
+        }
+        if ($this->selected_payment_for_delete) {
+            $this->selected_payment_for_delete->delete();
+            $this->dispatchBrowserEvent('alert', ['type' => 'success', 'message' => 'Success !']);
+        } else {
+            $this->dispatchBrowserEvent('alert', ['type' => 'error', 'message' => 'Selected payment not found !']);
         }
     }
 }
